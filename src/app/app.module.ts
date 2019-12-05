bf58fd097c2b1e375d5c36cf063c3f3ee130fa20
Sofia Paavola <paavola.sofia@gmail.com>
2019-12-05 16:39:17 +0000

    added bootstrap and new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,17 @@ import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { SearchSectionComponent } from './components/search-section/search-section.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
     NotFoundComponent,
-    HeaderComponent
+    HeaderComponent,
+    SearchSectionComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
