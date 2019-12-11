5a33ea933d6472b6167acefb470b58f2fca50f01
Sofia Paavola <paavola.sofia@gmail.com>
2019-12-11 16:46:49 +0000

    got Api key

---
+++ src/assets/data/quotes.ts
@@ -0,0 +1,10 @@
+// import { IBook } from "src/app/services/book/book.service";
+
+// export const emptyIBook = {
+//   id: "",
+//   title: "",
+//   pageCount: 0,
+//   description: "",
+//   categories: [],
+//   publishedDate: ""
+// };
